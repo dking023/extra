409754e9f9c4dd07fdef8e260ac75941a7209c18
radhe2244 <rkk812891@gmail.com>
2025-11-10 15:33:59 +0530

    Remove unused functions and clean up code

--- pr27.c
+++ pr27.c
@@ -1,20 +1,4 @@
-#include<stdio.h>
-void fibonaki();
-void printmonthsaving();
-void main()
-{
-    fibonaki();
-}
-void fibonaki()
-{
-    printmonthsaving();
-}
-void printmonthsaving()
-{
-    int i,sum=0,n,temp,n1=0,n2=1,ans=0;
-    printf("= = = = = = = = = = = = = ");
-    printf("How many month you saving:");
-    scanf("%d",&n);
+
     printf("Month 1 saving=0 rupee\nMonth 2 saving=1 rupee\n");
     for(i=2;i<=n;i++)
     {
@@ -30,3 +14,4 @@ void printmonthsaving()
     printf("\n           Total saving amount month saving=%d                      ",ans);
     printf("\n           = = = = = = = = = = = = = = = = = =                  \n");
 }
+
